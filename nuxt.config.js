909a4b25fefa7739a3bc74246d648391d81421a5
Yong Jin, Cho <drajin.cho@bosornd.com>
2019-01-09 07:54:33 +0900

    package-lock.json removed in git

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js + Vuetify.js project' }
+      { hid: 'description', name: 'description', content: 'http://www.bosornd.com' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
